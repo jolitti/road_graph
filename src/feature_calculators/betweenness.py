755ca8e6864440936765bf59e2d5520021acae78
Eddi3k <96292426+Eddi3k@users.noreply.github.com>
2024-12-16 13:26:55 +0100

    Added my old contributions

--- src/feature_calculators/betweenness.py
+++ src/feature_calculators/betweenness.py
@@ -6,15 +6,15 @@ import math
 import random
 
 import igraph as ig
-#import time        #to measure the time to compute features
+import time        #to measure the time to compute features
 
 def calculate_feature(graph:ig.Graph) -> list[float]:
-    #start = time.time()
+    start = time.time()
     vertex_betweenness = graph.betweenness()
-    #end = time.time()
+    end = time.time()
     print(vertex_betweenness)
-    #print("------------------------------------------------")
-    #print(end - start, "seconds")
+    print("------------------------------------------------")
+    print(end - start, "seconds")
     return []
 
 
